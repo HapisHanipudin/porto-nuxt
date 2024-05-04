64965588172969eeca8670f8fdb996498ac7ff19
Hapis Hanipudin <hapis.hanipuddin@gmail.com>
2024-05-04 11:13:51 +0700

    au ah

--- components/nav/index.vue
+++ components/nav/index.vue
@@ -6,8 +6,20 @@
       <NuxtLink to="/projects">Projects</NuxtLink>
       <NuxtLink to="/contact">Contact</NuxtLink>
       <NuxtLink to="/dashboard">Dashboard</NuxtLink>
-      <div :class="{ 'translate-x-0': route.path === '/' }" class="absolute top-1/2 -left-3 rounded-xl bg-zinc-900 -z-10 p-3 px-3 -translate-y-1/2 transition-all duration-200 ease-in-out">
-        <div :class="{ 'w-[44.63px]': route.path === '/' }" class="h-4 transition-all duration-200 ease-in-out"></div>
+      <div
+        :class="{
+          'translate-x-0': route.path === '/',
+          'translate-x-[calc(44.63px+24px)]': route.path === '/about',
+          'translate-x-[calc(45.42px+24px+44.63px+24px)]': route.path === '/projects',
+          'translate-x-[calc(61.47px+24px+45.42px+24px+44.63px+24px)]': route.path === '/contact',
+          'translate-x-[calc(60.14px+24px+61.47px+24px+45.42px+24px+44.63px+24px)]': route.path === '/dashboard',
+        }"
+        class="absolute top-1/2 -left-3 rounded-xl bg-zinc-900 -z-10 p-3 px-3 -translate-y-1/2 transition-all duration-200 ease-in-out"
+      >
+        <div
+          :class="{ 'w-[44.63px]': route.path === '/', 'w-[45.42px]': route.path === '/about', 'w-[61.47px]': route.path === '/projects', 'w-[60.14px]': route.path === '/contact', 'w-[82.47px]': route.path === '/dashboard' }"
+          class="h-4 transition-all duration-200 ease-in-out"
+        ></div>
       </div>
     </nav>
   </div>
