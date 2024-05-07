33ef437633a1dcc9bc33e03696ae9ad656e7fa57
Hapis Hanipudin <hapis.hanipuddin@gmail.com>
2024-05-07 17:02:16 +0700

    fixed

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="min-h-screen flex justify-center font-sans w-full text-white bg-dark-800">
     <div class="container m-10 transition-all duration-300 ease-in-out flex max-lg:flex-col gap-4">
-      <div class="lg:min-w-[30%] transition-all duration-300 ease-in-out">
+      <div class="min-w-[30%] transition-all duration-300 ease-in-out">
         <Sidebar />
       </div>
       <div class="flex h-min overflow-hidden transition-all duration-300 ease-in-out flex-col lg:flex-grow bg-zinc-900 rounded-3xl">
